43e06957b74e757b4e53bedbbceeb57ca8f394d3
Nkululeko Cyril Cele <nkululeko.cele@gmail.com>
2023-08-11 23:16:13 +0200

    Add JavaScript

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello, world!");
+/*document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('newsletterForm');
+    const successMessage = document.getElementById('successMessage');
+    const confirmEmailSpan = document.getElementById('confirmEmail');
+    const dismissBtn = document.getElementById('dismissBtn');
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const emailInput = document.getElementById('email');
+        const email = emailInput.value;
+
+        if (email && isValidEmail(email)) {
+            confirmEmailSpan.textContent = email;
+            successMessage.style.display = 'block';
+        } else {
+            alert('Please enter a valid email address.');
+        }
+    });
+
+    dismissBtn.addEventListener('click', () => {
+        successMessage.style.display = 'none';
+    });
+
+    const isValidEmail = email => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+});*/
